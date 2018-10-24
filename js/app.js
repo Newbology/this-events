2ae644f33c6f5396e34dc2757e445cc147b273ad
kevin tauzer <kevintauzer97@gmail.com>
2018-10-23 14:31:02 -1000

    exercise #2

--- js/app.js
+++ js/app.js
@@ -5,7 +5,21 @@
 
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+var pElemA=document.getElementsByClassName('name');
 
+for(var i=0; i<pElemA.length; i++){
+    pElemA[i].addEventListener('click', showMenu)
+}
+
+
+function showMenu(){
+    var showMenu=this.querySelectorAll('.menu')[0];
+    if(showMenu.style.display ==='block'){
+        shownMenu.style.display='none'
+    }else{
+        showMenu.style.display='block';
+    }
+}
 
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/
 
